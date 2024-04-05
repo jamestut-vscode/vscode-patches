58b588a91d3add58f73d07f609fe86a0dc856b80
James <aslan.jnn@gmail.com>
2024-04-06 08:35:14 +1100

    Use JSON for version info

--- packaging/scripts/doctorplugins/version.py
+++ packaging/scripts/doctorplugins/version.py
@@ -1,13 +1,20 @@
+import json
+
 def pre_run(doctorfn):
     print("Updating version string ...")
-    target_version = get_target_version()
+    product_version, ipc_version = get_target_versions()
     with doctorfn('package.json') as data:
-        if data[0]['version'] == target_version:
-            print("Version string already set. Skipping.")
-            return
-        data[0]['version'] = target_version
-        data[1] = True
+        if data[0]['version'] != product_version:
+            print("Updating package version string ...")
+            data[0]['version'] = product_version
+            data[1] = True
+    with doctorfn('product.json') as data:
+        if data[0].get('ipcVersion') != ipc_version:
+            print("Updating IPC version string ...")
+            data[0]['ipcVersion'] = ipc_version
+            data[1] = True
 
-def get_target_version():
-    with open('version.txt', 'r') as f:
-        return f.read().strip()
+def get_target_versions():
+    with open('version.json', 'r') as f:
+        d = json.load(f)
+    return d['productVersion'], d['ipcVersion']
