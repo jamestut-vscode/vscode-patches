62456a91a80819563d9f31acec1f85b5c6353398
James <aslan.jnn@gmail.com>
2026-02-13 15:48:09 +1100

    Fix portable-modifier.sh script so it works on normal sh rather than bash

--- scripts/reh-supplemental/portable-modifier.sh
+++ scripts/reh-supplemental/portable-modifier.sh
@@ -1,5 +1,5 @@
 TARGET_SYSROOT="$ROOT"/sysroot
-if [[ "$("$ROOT/patchelf" --print-rpath "$ROOT/node")" != "$TARGET_SYSROOT" ]]
+if [ "$("$ROOT/patchelf" --print-rpath "$ROOT/node")" != "$TARGET_SYSROOT" ]
 then
     "$ROOT/patchelf" --set-rpath "$TARGET_SYSROOT" "$ROOT/node"
     "$ROOT/patchelf" --set-interpreter "$TARGET_SYSROOT/$$LD_NAME$$" "$ROOT/node"
